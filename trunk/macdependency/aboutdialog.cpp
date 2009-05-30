1516612b04cdd9d143c7a862be28dea444d62bd0
konrad_w@gmx.de <konrad_w@gmx.de@91765166-4476-11de-952f-710615d9d0d5>
2009-05-30 19:41:31 +0000

    initial commit of whole project

---
+++ trunk/macdependency/aboutdialog.cpp
@@ -0,0 +1,30 @@
+#include "aboutdialog.h"
+#include "ui_aboutdialog.h"
+
+AboutDialog::AboutDialog(QWidget *parent) :
+    QDialog(parent),
+    m_ui(new Ui::AboutDialog)
+{
+    m_ui->setupUi(this);
+}
+
+AboutDialog::~AboutDialog()
+{
+    delete m_ui;
+}
+
+void AboutDialog::changeEvent(QEvent *e)
+{
+    QDialog::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        m_ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+
+void AboutDialog::on_aboutQtButton_clicked() {
+    qApp->aboutQt();
+}
