0ef26534c51dfd4cd003bdb10918bbea8121af8a
李通洲 <carter.li@eoitek.com>
2021-10-31 15:40:59 +0800

    Show Arm 64 instead of UNDEFINED

--- MachO/machoheader.cpp
+++ MachO/machoheader.cpp
@@ -64,6 +64,15 @@ MachOHeader::CpuType MachOHeader::getCpuType(unsigned int cpu) {
         case CPU_TYPE_X86_64:
             cpuType = MachOHeader::CpuTypeX8664;
             break;
+        case CPU_TYPE_ARM:
+            cpuType = MachOHeader::CpuTypeArm;
+            break;
+        case CPU_TYPE_ARM64:
+            cpuType = MachOHeader::CpuTypeArm64;
+            break;
+        case CPU_TYPE_ARM64_32:
+            cpuType = MachOHeader::CpuTypeArm6432;
+            break;
         default:
             cpuType = MachOHeader::CpuTypeOther;
     }
